ae7c5b4accc765ee497d683c6dc7d5dabb620ed2
Ubunut <jason@ubuntu.(none)>
2012-08-07 13:09:19 -0700

    Comments with correct format

--- Arduino.py
+++ Arduino.py
@@ -1,42 +1,40 @@
-"""
-Arduino is the code that will be replicated on the actual microcontroller.
-Any SET functions will NOT be translated into the real application,
-but are present for use in simulation to present the sensors with data.
-"""
 class Arduino:
+    """
+    Arduino is the code that will be replicated on the actual microcontroller.
+    Any SET functions will NOT be translated into the real application,
+    but are present for use in simulation to present the sensors with data.
+    """
     def __init__(self):
         
         return
     
-    """
-    Read X acceleration in the world cordinate system
-    This is a computed value based on sensors
-    """
+    
     def getXAcceleration(self):
+        """
+        Read X acceleration in the world cordinate system
+        This is a computed value based on sensors
+        """
         
         return
     
-    """
-    Read Y acceleration world cordinate system
-    This is a computed value based on sensors
-    """
     def getYAcceleration(self):
-        
+        """
+        Read Y acceleration world cordinate system
+        This is a computed value based on sensors
+        """
         return
     
-    """
-    Read Z acceleration world cordinate system
-    This is a computed value based on sensors
-    """
     def getZAcceleration(self):
-        
+        """
+        Read Z acceleration world cordinate system
+        This is a computed value based on sensors
+        """
         return
 
-    """
-    Read Z distance sensor
-    """
     def getZDistance(self):
-        
+        """
+        Read Z distance sensor
+        """
         return
 
     """
@@ -45,21 +43,19 @@ class Arduino:
     --------------------------------------------------------------------------------
     """
 
-    """
-    Updates all the "real" data that the sensors will read.  This data will be used 
-    by the ardiuno to calculate its precieved measurements.
-    INPUTS: data
-    """
     def setSensorData(self):
-        
+        """
+        Updates all the "real" data that the sensors will read.  This data will be used 
+        by the ardiuno to calculate its precieved measurements.
+        INPUTS: data
+        """
         return
     
-    """
-    Insert the user input that will come from a WII remote or keyboard
-    from the current user.  This will enable real simulation from a 
-    remote control in the real QuadCopter
-    """
     def setUserInput(self):
-        
+        """
+        Insert the user input that will come from a WII remote or keyboard
+        from the current user.  This will enable real simulation from a 
+        remote control in the real QuadCopter
+        """
         return
     
